54b72028b1e396feade3853aab8da746f1d90427
derrylpriyambodo24 <derrylpriyambodo24@gmail.com>
2025-12-23 22:33:08 +0700

    Fix target column name and update comments
    Updated comments and corrected target column name in data processing.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,28 +6,29 @@ import mlflow
 import mlflow.sklearn
 
 # 1. Load Dataset
-# Pastikan nama file sesuai dengan yang ada di folder MLProject
-df = pd.read_csv('water_potability_preprocessing.csv') 
+# Pastikan nama file sesuai
+df = pd.read_csv('water_potability_preprocessing.csv')
 
-# Pisahkan fitur dan target (Sesuaikan 'Target' dengan nama kolom label kamu)
-X = df.drop(columns=['Target']) 
-y = df['Target']
+# 2. Pisahkan Fitur dan Target
+# PERBAIKAN DI SINI: Gunakan 'Potability' sebagai nama kolom yang didrop dan diambil
+X = df.drop(columns=['Potability'])
+y = df['Potability']
 
 # Split Data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 2. Set Eksperimen & Autolog
+# 3. Set Eksperimen & Autolog
 mlflow.set_experiment("Eksperimen_Basic_Derryl")
 
 with mlflow.start_run():
-    # PENTING: Aktifkan log_models=True sesuai request reviewer
+    # Aktifkan log_models=True
     mlflow.sklearn.autolog(log_models=True)
 
-    # 3. Training Model
+    # 4. Training Model
     rf = RandomForestClassifier(n_estimators=100, random_state=42)
     rf.fit(X_train, y_train)
 
-    # 4. Evaluasi (Metrics akan otomatis ter-log oleh autolog, tapi print juga oke)
+    # 5. Evaluasi
     y_pred = rf.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     print(f"Accuracy: {acc}")
